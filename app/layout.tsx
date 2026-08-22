65753dd71c3fc0c42d19eea0b67ac0a9c4d85de2
Za <z@Zas-MacBook-Air.local>
2026-08-22 09:52:34 -0700

    Use Cava chocolate brown storefront palette

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./builder-overrides.css";
 import "./store-overrides.css";
+import "./color-overrides.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
