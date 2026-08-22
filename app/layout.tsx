f9a516c245ff814582d94e076796b236390f224a
Za <z@Zas-MacBook-Air.local>
2026-08-22 09:40:01 -0700

    Match Gigi Pip storefront shell

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./builder-overrides.css";
+import "./store-overrides.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
